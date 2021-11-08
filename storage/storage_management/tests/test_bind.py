69d22ca11e9c90aeb6fbfa6e521ba2b9a5284132
etd <sirily1997@gmail.com>
2021-11-08 19:49:00 +0800

    add more tests

--- storage/storage_management/tests/test_bind.py
+++ storage/storage_management/tests/test_bind.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIRequestFactory, force_authenticate
-
+from django.utils.http import urlquote
 from ..models import Owner, Item, MachineType, Location, DetailPosition
 from ..util_views import BindDeviceView, GetOwnerDevicesView
 
@@ -49,7 +49,7 @@ class TestBind(APITestCase):
         self.assertEqual(Owner.objects.count(), 0)
 
     def test_bind_device_with_owner(self):
-        owner =  Owner.objects.create(user_id="my_owner")
+        owner = Owner.objects.create(user_id="my_owner")
         Item.objects.create(name="Test Object", machine_type=self.machine_type, location=self.location,
                             detail_position=self.position, qr_code="test_test", owner=owner)
         factory = APIRequestFactory()
@@ -59,6 +59,19 @@ class TestBind(APITestCase):
         response = view(request)
         self.assertEqual(response.status_code, 500)
 
+    def test_bind_device_with_owner_with_special_character(self):
+        user_name = "+1-12345678"
+        Item.objects.create(name="Test Object", machine_type=self.machine_type, location=self.location,
+                            detail_position=self.position, qr_code="test_test", owner=None)
+        factory = APIRequestFactory()
+        request = factory.post("/bind/", data={"user": user_name, "device": "test_test"})
+        force_authenticate(request, user=self.user)
+        view = BindDeviceView.as_view()
+        response = view(request)
+
+        owner = Owner.objects.filter(user_id=user_name)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(owner.exists())
 
 
 class TestGetUserDevices(APITestCase):
@@ -67,13 +80,26 @@ class TestGetUserDevices(APITestCase):
         self.machine_type = MachineType.objects.create(name="test")
         self.location = Location.objects.create(country="a")
         self.owner = Owner.objects.create(user_id="test")
+        self.owner2 = Owner.objects.create(user_id="+1-1234567")
         self.position = DetailPosition.objects.create(position="test position")
         Item.objects.create(name="Test Object", machine_type=self.machine_type, location=self.location,
                             detail_position=self.position, qr_code="test_device", owner=self.owner)
 
+        Item.objects.create(name="Test Object2", machine_type=self.machine_type, location=self.location,
+                            detail_position=self.position, qr_code="test_device2", owner=self.owner2)
+
     def test_get_user_devices(self):
         factory = APIRequestFactory()
         request = factory.get("?user=test")
+        force_authenticate(request, user=self.user)
+        view = GetOwnerDevicesView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_user_devices_with_specital_character(self):
+        user_name = "+1-1234567"
+        factory = APIRequestFactory()
+        request = factory.get(f"?user={urlquote(user_name)}")
         force_authenticate(request, user=self.user)
         view = GetOwnerDevicesView.as_view()
         response = view(request)
